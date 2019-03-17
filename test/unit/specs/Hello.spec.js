2dfab5b2e9b8f5b41149966e24d7dad0c0983d00
jilon <277651205@qq.com>
2019-03-17 22:31:53 +0800

    files

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -5,7 +5,6 @@ describe('Hello', () => {
   it('组件render', () => {
     const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js Appsss')
+    expect(vm.$el.querySelector('.hello h1').textContent).to.equal('Welcome to Your Vue.js Appsss')
   })
 })
--- test/unit/specs/List.spec.js
+++ test/unit/specs/List.spec.js
@@ -3,41 +3,31 @@ import List from '../../../src/components/List.vue';
 import Vue from 'vue';
 
 describe('List', () => {
-
   it('测试1', () => {
     const Constructor = Vue.extend(List);
     const ListComponent = new Constructor().$mount();
-
     expect(ListComponent.$el.textContent).to.contain('play gamess');
   })
-
   it('测试2', () => {
     const Constructor = Vue.extend(List);
     const ListComponent = new Constructor().$mount();
-
     ListComponent.newItem = 'brush my teethssss';
-
     // simulate click event
     const button = ListComponent.$el.querySelector('button');
     const clickEvent = new window.Event('click');
     button.dispatchEvent(clickEvent);
     ListComponent._watcher.run();
-
     // assert list contains new item
     expect(ListComponent.$el.textContent).to.contain('brush my teeth');
     expect(ListComponent.listItems).to.contain('brush my teeth');
   })
-
   it('测试3', () => {
     const ListComponent = mount(List);
-
     ListComponent.setData({
       newItem: 'brush my teeth',
     });
-
     const button = ListComponent.find('button')[0];
     button.dispatch('click');
-
     expect(ListComponent.text()).to.contain('brush my teeth');
     expect(ListComponent.data().listItems).to.contain('brush my teeth');
   })
